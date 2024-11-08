f426164f0698e8b092e08b7c3756a041f0b98b07
joaoduda0 <joaoeduardomc@hotmail.com>
2024-11-07 23:11:09 -0300

    santa cruz joao

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 print ("qualquer coisa serve")
 print("OLÁ MUNDO")
-<<<<<<< HEAD
-print('Joao')
-=======
 print("teste saldanha1")
->>>>>>> 788164c4338a92e0ae8fbdfb4d016b90aa89b9ff
+
+
+print("teste jao")
